fb1cfb093ac9f888ee57a91507241e96f2955181
Victors081 <victorsarmiento8@gmail.com>
2025-01-20 22:24:48 -0500

    Agregar get_r_installer()

--- R/get_r_installer.R
+++ R/get_r_installer.R
@@ -1,3 +1,13 @@
+#' Get R installer
+#'
+#' @param x A character vector with one element.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' get_r_installer(x)
 get_r_installer <- function(x) {
   print(paste0(x,"R ha sido instaldo"))
 }
